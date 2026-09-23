6542da6a2e54714ceb8c5703d86d79e50af266ae
Anisa-abdikadir <anizaabdikaadir@gmail.com>
2026-09-23 16:10:57 +0300

    Fix build errors

--- FrontEnd/src/Pages/Projects.jsx
+++ FrontEnd/src/Pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import HeroProject from '../Project/HeroProject'
-import NavBar from '../Components/Navbar'
+import NavBar from "../Components/NavBar"
 import P_S_TWO from '../Project/P_S_TWO'
 import Footer from '../Components/Footer'
 const Projects = () => {
